2d63f0890bb11898eb601a6c0a0275d2c51ab1d5
Nick Giangreco <nick.giangreco@gmail.com>
2023-07-01 17:54:05 -0400

    basic mccv shiny layout

--- app.py
+++ app.py
@@ -1,17 +1,30 @@
 from shiny import App, render, ui
 
 app_ui = ui.page_fluid(
-    ui.h2("Hello Shiny!"),
-    ui.input_slider("n", "N", 0, 100, 20),
-    ui.output_text_verbatim("txt"),
+    ui.row(ui.h1('Monte Carlo Cross Validation')),
+    ui.layout_sidebar(
+        ui.panel_sidebar(
+            ui.output_ui('upload_data'),
+            ui.input_action_button('run_model','Run MCCV'),
+            ui.output_ui('parameterize_model')
+        ),
+        ui.panel_main(
+            ui.output_ui('show_model')
+        )
+    )
 )
 
 
 def server(input, output, session):
     @output
-    @render.text
-    def txt():
-        return f"n*2 is {input.n() * 2}"
+    @render.ui
+    def parameterize_model():
+        ui.input_slider("n", "N", 0, 100, 20)
+        
+    @output
+    @render.ui
+    def show_model():
+        return 'model run'
 
 
 app = App(app_ui, server)
