a8b66cedfbb37f2d540092b73b670d4123c2c77b
Nick Giangreco <nick.giangreco@gmail.com>
2023-07-03 15:27:05 -0400

    added description and removed heading

--- app.py
+++ app.py
@@ -7,6 +7,12 @@ from mccv_parameters import mccv_parameters_ui, mccv_parameters_server
 app_ui = ui.page_fluid(
     shinyswatch.theme.flatly(),
     ui.panel_title('Monte Carlo Cross Validation'),
+    ui.markdown(
+        '''
+        Evidentiary and interpretable prediction. Learn more at [mccv.nickg.bio](mccv.nickg.bio).
+        
+        Use this app to simulate data, classify data into two groups, run MCCV, and inspect prediction results.
+        '''),
     generate_data_ui('simulate'),
     ui.layout_sidebar(
         sidebar = mccv_parameters_ui('mccv_parameters'),
--- generate_data.py
+++ generate_data.py
@@ -1,18 +1,19 @@
 from shiny import App, reactive, render, ui, module
 from htmltools import css
+import seaborn as sns
 
 @module.ui
 def generate_data_ui(label: str = "simulate"):
     return     ui.panel_main(
         {"style": "border: 1px solid #ccc; border-radius: 5px; margin: 5px 0;"},
-        ui.h2('Generate Univariate Data'),
         ui.div(
             ui.input_selectize('dist','Distribution Type',choices = ['Normal','T','Beta'],multiple=False,width = '200px'),
             ui.output_ui('dist_params'),
             style=css(
             display="flex", justify_content="center", align_items="center", gap="2rem"
             )
-        )
+        ),
+        ui.output_plot('dist_plot')
     )
 
 @module.server
@@ -22,3 +23,17 @@ def generate_data_server(input, output, session):
     @render.ui
     def dist_params():
         return 'Placeholder'
+    
+    @output
+    @render.plot
+    def dist_plot():
+        sns.set_theme(style="ticks", palette="pastel")
+
+        # Load the example tips dataset
+        tips = sns.load_dataset("tips")
+
+        # Draw a nested boxplot to show bills by day and time
+        sns.boxplot(x="day", y="total_bill",
+                    hue="smoker", palette=["m", "g"],
+                    data=tips)
+        sns.despine(offset=10, trim=True)
