f1e53c308fb7bf557a102fc5510b77d3c9e6f5fe
Nick Giangreco <nick.giangreco@gmail.com>
2023-07-07 22:34:47 -0400

    added effect size slider and array generator fcn

--- generate_data.py
+++ generate_data.py
@@ -5,24 +5,43 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 
+def generate_arrays(class1_size, effect_size, input_array):
+    '''
+    We calculate the lengths of the two classes based on the class1_size input, ensuring that their sum equals the length of the input_array.
+    We calculate the mean difference between the two output arrays by multiplying the effect_size input with the standard deviation of the input_array.
+    We create the class1_array by taking the first class1_length elements of the input_array and adding the mean difference.
+    We create the class0_array by taking the remaining elements of the input_array and subtracting the mean difference.
+    Finally, we return the two output arrays: class1_array and class0_array.
+    '''
+    class1_length = int(class1_size * len(input_array))
+    class0_length = len(input_array) - class1_length
+    
+    mean_diff = effect_size * np.std(input_array)
+    
+    class1_array = input_array[:class1_length] + mean_diff
+    class0_array = input_array[class1_length:] - mean_diff
+    
+    return class1_array, class0_array
+
 @module.ui
 def generate_data_ui(label: str = "simulate"):
     return  ui.layout_sidebar(
                 sidebar = ui.div(
                     ui.input_slider('n','N',
-                                    min=50,max=500,step=50,value=50,
+                                    min=50,max=500,step=50,value=100,
                                     ticks=False,width='200px'),
                     ui.input_selectize('dist','Distribution Type',
                                     choices = {'normal' : 'Normal','beta' : 'Beta'},multiple=False,width = '200px'),
-                    ui.input_slider('param1','Parameter 1',min=0,max=1,step=0.1,value=0),
-                    ui.input_slider('param2','Parameter 2',min=0,max=1,step=0.1,value=0),
-                    ui.input_slider('prop_class1',label='Proportion of Class 1',min=0,max=1,value=0.5,step=0.1)
+                    ui.input_slider('param1','Parameter 1',min=0,max=1,step=0.1,value=0,ticks=False,width='200px'),
+                    ui.input_slider('param2','Parameter 2',min=0,max=1,step=0.1,value=0,ticks=False,width='200px'),
+                    ui.input_slider('prop_class1',label='Proportion of Class 1',min=0.2,max=0.8,value=0.5,step=0.1,ticks=False,width='200px'),
+                    ui.input_slider('std_diff',label='Effect Size of Class 1',min=0,max=1,value=0,step=0.1,ticks=False,width='200px')
                 ),
                 main = ui.navset_tab_card(
-                    ui.nav('Table',
-                           ui.output_data_frame('dist_table')),
                     ui.nav('Plot',
-                           ui.output_plot('dist_plot'))
+                           ui.output_plot('dist_plot')),
+                    ui.nav('Table',
+                           ui.output_data_frame('dist_table'))
                     )
             )
 
@@ -53,11 +72,9 @@ def generate_data_server(input, output, session):
         arr = dist_func()(input.param1(),
                             input.param2(),
                             size=input.n())
-        statuses = np.ones(input.n())
-        arr_sorted = np.sort(arr)
-        n_class0 = int(input.n()*(1-input.prop_class1()))
-        statuses[np.arange(1,n_class0)] = 0
-        return pd.DataFrame({'result' : list(arr_sorted),'status' : list(statuses)})
+        array1, array0 = generate_arrays(input.prop_class1(),input.std_diff(), arr)
+        return pd.DataFrame({'result' : np.concatenate([array1,array0]),
+                             'class' : np.concatenate([np.ones(len(array1)),np.zeros(len(array0))])})
         
     @output
     @render.data_frame
@@ -70,4 +87,4 @@ def generate_data_server(input, output, session):
     @output
     @render.plot
     def dist_plot():
-        return sns.histplot(data_generator(),x='result',hue='status')
+        return sns.histplot(data_generator(),x='result',hue='class')
