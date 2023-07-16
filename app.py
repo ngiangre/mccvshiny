16aefde24f67c54d003ec5c9d1ddc177e4c799b4
Nick Giangreco <nick.giangreco@gmail.com>
2023-07-16 19:38:51 -0400

    made module for mccv_results

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from shiny import App, reactive, render, ui
 import shinyswatch
 import mccv
 from generate_data import generate_data_ui, generate_data_server
-from mccv_parameters import mccv_parameters_ui, mccv_parameters_server
+from mccv_results import mccv_results_ui, mccv_results_server
 
 import pandas as pd
 
@@ -16,36 +16,13 @@ app_ui = ui.page_fluid(
         Use this app to simulate data, classify data into two groups, run MCCV, and inspect prediction results.
         '''),
     generate_data_ui('simulate'),
-    ui.layout_sidebar(
-        sidebar = ui.panel_sidebar(
-            ui.input_action_button('run_model','Run MCCV',class_="btn-primary"),
-            mccv_parameters_ui('mccv_parameters'),
-            width = 2
-        ),
-        main = ui.navset_tab_card(
-                ui.nav('Tables',ui.output_data_frame('tables')),
-                ui.nav('Plots',ui.output_ui('plots'))
-            )
-    )
+    mccv_results_ui('mccv_results')
 )
 
 def server(input, output, session):
     mccv_obj = mccv.mccv()
     generate_data_server('simulate',mccv_obj)
     mccv_parameters_server('mccv_parameters',mccv_obj)
-    
-    @reactive.Calc
-    @reactive.event(input.run_model)
-    def mccv_data():
-        mccv_obj.run_mccv()
-        return mccv_obj.mccv_data
-            
-    @output
-    @render.data_frame
-    @reactive.event(mccv_data)
-    def tables():
-        return render.DataGrid(
-            mccv_data()['Performance'].groupby(['model','metric'])['value'].mean().reset_index()
-        )
+    mccv_results_server('mccv_results',mccv_obj)
 
 app = App(app_ui, server)
---
+++ mccv_results.py
@@ -0,0 +1,33 @@
+from shiny import App, reactive, render, ui, module
+from htmltools import css
+
+from mccv_parameters import mccv_parameters_ui, mccv_parameters_server
+
+@module.ui
+def mccv_results_ui(label: str = 'mccv_parameters'):
+    return ui.layout_sidebar(
+        sidebar = ui.panel_sidebar(
+            ui.input_action_button('run_model','Run MCCV',class_="btn-primary"),
+            mccv_parameters_ui('mccv_parameters'),
+            width = 2
+        ),
+        main = ui.navset_tab_card(
+                ui.nav('Tables',ui.output_data_frame('tables')),
+                ui.nav('Plots',ui.output_ui('plots'))
+            )
+    )
+@module.server
+def mccv_results_server(input,output,session,mccv_obj):
+    @reactive.Calc
+    @reactive.event(input.run_model)
+    def mccv_data():
+        mccv_obj.run_mccv()
+        return mccv_obj.mccv_data
+            
+    @output
+    @render.data_frame
+    @reactive.event(mccv_data)
+    def tables():
+        return render.DataGrid(
+            mccv_data()['Performance'].groupby(['model','metric'])['value'].describe(percentiles=[0.025,0.975]).reset_index()
+        )
