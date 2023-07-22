155907445f4ccc357aff1950942f34caf3c6b2d0
Nick Giangreco <nick.giangreco@gmail.com>
2023-07-22 07:28:21 -0400

    added navigation with link to github

--- app.py
+++ app.py
@@ -4,19 +4,25 @@ import mccv
 from generate_data import generate_data_ui, generate_data_server
 from mccv_results import mccv_results_ui, mccv_results_server
 
-import pandas as pd
-
 app_ui = ui.page_fluid(
     shinyswatch.theme.flatly(),
     ui.panel_title('Monte Carlo Cross Validation'),
-    ui.markdown(
-        '''
-        Evidentiary and interpretable prediction. Learn more at [mccv.nickg.bio](mccv.nickg.bio).
-        
-        Use this app to simulate data, classify data into two groups, run MCCV, and inspect prediction results.
-        '''),
-    generate_data_ui('simulate'),
-    mccv_results_ui('mccv_results')
+    ui.navset_tab_card(
+        ui.nav('MCCV',
+            ui.markdown(
+                '''
+                Evidentiary and interpretable prediction. Learn more at [mccv.nickg.bio](mccv.nickg.bio).
+                
+                Use this app to simulate data, classify data into two groups, run MCCV, and inspect prediction results.
+                '''),
+            generate_data_ui('simulate'),
+            mccv_results_ui('mccv_results')
+            ),
+        ui.nav_spacer(),
+        ui.nav_control(
+            ui.a("Github", href="https://github.com/ngiangre/mccvshiny", target="_blank")
+        )
+    )
 )
 
 def server(input, output, session):
