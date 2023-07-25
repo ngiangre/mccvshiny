2efdf2340ecc2bdd238adbd96c90dd6195b8adc8
Nick Giangreco <nick.giangreco@gmail.com>
2023-07-24 20:41:41 -0400

    revised intro text in app

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app_ui = ui.page_fluid(
         ui.nav('MCCV',
             ui.markdown(
                 '''
-                Evidentiary and interpretable prediction. Learn more at [mccv.nickg.bio](mccv.nickg.bio).
+                Showcase of the mccv python package employing Monte Carlo Cross Validation (MCCV). Learn more at [mccv.nickg.bio](mccv.nickg.bio). This app was developed purely as an educational and learning resource/outlet. 
                 
                 Use this app to simulate data, parameterize learning with MCCV (limited to LR currently), run MCCV, and observe prediction results. Gray region in MCCV result plots indicate permutation distribution for comparison.
                 '''),
